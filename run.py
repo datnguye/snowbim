1ea733524921c0efd47f03222ba75d1875a5f9cc
datnguye <datnguyen.it09@gmail.com>
2021-05-25 17:35:01 +0700

    upgrade_bim #TODO

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
-from snowbim import snowbim
-changes = snowbim.get_schema_changes()
-print(changes)
+# from snowbim import snowbim
+# changes = snowbim.get_schema_changes()
+# print(changes)
+
+from snowbim.engines import bimengine
+r = bimengine.upgrade_bim(file_path='', out_path=None, changes=[])
+print(type(r[0]))
+print(type(r[1]))
--- snowbim/engines/bimengine.py
+++ snowbim/engines/bimengine.py
@@ -1,5 +1,30 @@
-def load_bim(file_path):
-    pass
+import json
+import os
 
-def upgrade_bim(file_path, out_path, changes):
-    pass
+def load_bim(file_path:str=None):
+    '''
+    Read .bim file as json object. Then, convert it to dict object
+    Return a tuple (code, dict, messasge)
+    '''
+    bim = None
+    if os.path.exists(file_path):
+        return (-1, {}, 'File not found')
+
+    with open(file_path, 'r') as f:
+        bim = json.load(f)
+    return (0, bim, None)
+    
+
+def upgrade_bim(file_path:str=None, out_path:str=None, changes:list=[]):
+    '''
+    With the changes object, make changes for dict's attributes related
+    Return a tuple (code, dict, messasge)
+    '''
+    input_bim = load_bim(file_path=file_path)
+    output_bim = {}
+    if input[0] != 0:
+        return (input[0], {}, input[2])
+    
+    # TODO
+
+    return (0, output_bim, None)
